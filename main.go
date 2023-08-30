892c3fdd5ea126337bfb07a839fbbebcb16dd37e
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-08-30 10:19:03 +0630

    sync.WaitGroup: wg.add() = len(services)

--- main.go
+++ main.go
@@ -12,10 +12,16 @@ func notify(services ...string) {
 	// Step-1: Declare the sync.WaitGroup
 	var wg sync.WaitGroup
 
+	// In either case, I recommend calling Add() outside of the concurrent code to ensure it
+	// runs immediately. If we were to instead place this inside of the goroutine it is possible
+	// that the program will get to the wg.Wait() line before the goroutine has run, in which case
+	// wg.Wait() won’t have anything to wait on and we will be in the same position we were in before.
+	// This is shown on the Go Playground here: https://play.golang.org/p/Yl4f_5We6s7
+	wg.Add(len(services))
 	for _, service := range services {
 
 		// Step-2: Add to the WaitGroup queue
-		wg.Add(1)
+		// wg.Add(1)
 		go func(s string) {
 			fmt.Printf("Starting to notifying %s...\n", s)
 			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
