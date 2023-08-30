6f63426f676849bd3fa543fcbbe614ebdd31c9ed
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-08-30 11:34:04 +0630

    Go Class:23 CSP,Goroutines, and Channels - 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yehtetmaungmaung/go-practice
+
+go 1.20
--- main.go
+++ main.go
@@ -1,42 +1,49 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"sync"
+	"log"
+	"net/http"
 	"time"
 )
 
-func notify(services ...string) {
+type result struct {
+	url     string
+	err     error
+	latency time.Duration
+}
 
-	// Step-1: Declare the sync.WaitGroup
-	var wg sync.WaitGroup
+func get(url string, ch chan<- result) {
+	start := time.Now()
 
-	// In either case, I recommend calling Add() outside of the concurrent code to ensure it
-	// runs immediately. If we were to instead place this inside of the goroutine it is possible
-	// that the program will get to the wg.Wait() line before the goroutine has run, in which case
-	// wg.Wait() won’t have anything to wait on and we will be in the same position we were in before.
-	// This is shown on the Go Playground here: https://play.golang.org/p/Yl4f_5We6s7
-	wg.Add(len(services))
-	for _, service := range services {
-
-		// Step-2: Add to the WaitGroup queue
-		// wg.Add(1)
-		go func(s string) {
-			fmt.Printf("Starting to notifying %s...\n", s)
-			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
-			fmt.Printf("Finished notifying %s...\n", s)
-
-			// Step-4: Inside each goroutine, mark items in the queue as done
-			wg.Done()
-		}(service)
+	if resp, err := http.Get(url); err != nil {
+		ch <- result{url, err, 0}
+	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t}
+		resp.Body.Close()
 	}
-
-	// Step-3: Tell our code to wait on the WaitGroup queue to reach zero before proceeding
-	wg.Wait()
-	fmt.Println("All services notified!")
 }
 
 func main() {
-	notify("Service-1", "Service-2", "Service-3", "Service-4")
+	results := make(chan result)
+	list := []string{
+		"https://www.google.com",
+		"https://amazon.com",
+		"https://nytimes.com",
+		"https://wsj.com",
+	}
+
+	for _, url := range list {
+		go get(url, results)
+	}
+
+	for range list {
+		r := <-results
+
+		if r.err != nil {
+			log.Printf("%-30s %s\n", r.url, r.err)
+		} else {
+			log.Printf("%-30s %s\n", r.url, r.latency)
+		}
+	}
 }
