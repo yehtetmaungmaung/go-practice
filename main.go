4cd95d8c941d02a5d2616ee6767b23af2b98be53
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-08-30 10:13:24 +0630

    sync.WaitGroup: After

--- main.go
+++ main.go
@@ -3,22 +3,34 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func notify(services ...string) {
+
+	// Step-1: Declare the sync.WaitGroup
+	var wg sync.WaitGroup
+
 	for _, service := range services {
+
+		// Step-2: Add to the WaitGroup queue
+		wg.Add(1)
 		go func(s string) {
-			fmt.Printf("Starting to notifing %s...\n", s)
+			fmt.Printf("Starting to notifying %s...\n", s)
 			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
 			fmt.Printf("Finished notifying %s...\n", s)
+
+			// Step-4: Inside each goroutine, mark items in the queue as done
+			wg.Done()
 		}(service)
 	}
+
+	// Step-3: Tell our code to wait on the WaitGroup queue to reach zero before proceeding
+	wg.Wait()
 	fmt.Println("All services notified!")
 }
 
 func main() {
-	notify("Service-1", "Service-2", "Service-3")
-	// Running this outputs "All services notified!" but we
-	// won't see any of the services outputting their finished messages!
+	notify("Service-1", "Service-2", "Service-3", "Service-4")
 }
