5e4d7f453f1af26e826b4bb037bf85136916be28
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-08-31 09:50:54 +0630

    Go Class:23 CSP,Goroutines, and Channels - 3

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-var nextID int
+var nextID = make(chan int)
 
+// Every request will read from nextID channel, then the counter go
+// routine will be able to send
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>You got %d!</h1>", nextID)
-	// This is bad!!!
-	// The next commit shows the proper way by using channel
-	nextID++
+	fmt.Fprintf(w, "<h1>You got %d!</h1>", <-nextID)
+}
+
+// The function will run forever, but nextID will be blocked until
+// handler reads from the nextID channel
+func counter() {
+	for i := 0; ; i++ {
+		nextID <- i
+	}
 }
 
 func main() {
+	go counter()
 	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
