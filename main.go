a1a26c746edd3f506537938ef6b8add42a8ac943
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-08-31 09:43:43 +0630

    Go Class:23 CSP,Goroutines, and Channels - 2

--- main.go
+++ main.go
@@ -1,49 +1,19 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-	"time"
 )
 
-type result struct {
-	url     string
-	err     error
-	latency time.Duration
-}
+var nextID int
 
-func get(url string, ch chan<- result) {
-	start := time.Now()
-
-	if resp, err := http.Get(url); err != nil {
-		ch <- result{url, err, 0}
-	} else {
-		t := time.Since(start).Round(time.Millisecond)
-		ch <- result{url, nil, t}
-		resp.Body.Close()
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>You got %d!</h1>", nextID)
+	// This is bad!!!
+	// The next commit shows the proper way by using channel
+	nextID++
 }
 
 func main() {
-	results := make(chan result)
-	list := []string{
-		"https://www.google.com",
-		"https://amazon.com",
-		"https://nytimes.com",
-		"https://wsj.com",
-	}
-
-	for _, url := range list {
-		go get(url, results)
-	}
-
-	for range list {
-		r := <-results
-
-		if r.err != nil {
-			log.Printf("%-30s %s\n", r.url, r.err)
-		} else {
-			log.Printf("%-30s %s\n", r.url, r.latency)
-		}
-	}
+	http.HandleFunc("/", handler)
 }
