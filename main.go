562e8b4332bb15896ceaa7500bdcb63c186890d8
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2023-09-04 11:36:15 +0630

    Prime Sieve template func

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
+// Prime Sieve implementation with go channels
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+func generator(limit int, ch chan<- int) {
 
-// Now: let's implement object oriented version of the previous version.
-
-type nextCh chan int
-
-// Every request will read from nextID channel, then the counter go
-// routine will be able to send
-func (ch nextCh) handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>You got %d!</h1>", <-ch)
 }
 
-// The function will run forever, but nextID will be blocked until
-// handler reads from the nextID channel
-func counter(ch chan<- int) {
-	for i := 0; ; i++ {
-		ch <- i
-	}
+func filter(src <-chan int, dest chan<- int, prime int) {
+
+}
+
+func sieve(limt int) {
+
 }
 
 func main() {
-	var nextID nextCh = make(chan int)
-	go counter(nextID)
-	http.HandleFunc("/", nextID.handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
